b73a790a057d323c475166b4d6c4ada159dadb4a
Nikhil Kumar <nbaghel12@gmail.com>
2022-10-10 13:56:21 +0530

    Updates

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,7 +59,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  console.log(session.line_items[0].price_data.unit_amount);
+  console.log(session.display_items);
+  console.log(session.display_items[0]);
+  console.log(session.display_items[0].price_data);
+  console.log(session.display_items[0].price_data.unit_amount);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
